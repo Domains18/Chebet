53edc7a7a1cb7b68e734c84e159a459d2fe91ff7
domains18 <gibsonsgibson88@gmail.com>
2023-01-17 13:34:09 +0300

    CreatableReactSelect

--- src/pages/NoteForm.tsx
+++ src/pages/NoteForm.tsx
@@ -1,21 +1,38 @@
 import React from 'react'
-import { Col, Form, Row, Stack } from 'react-bootstrap'
-
+import { Link } from 'react-router-dom'
+import { Button, Col, Form, Row, Stack } from 'react-bootstrap'
+import CreatableReactSelect from 'react-select/creatable'
 const NoteForm = () => {
-  return (
-    <Form>
-        <Stack gap={4}>
-            <Row>
-                <Col>
-                    <Form.Group controlId="title">
-                          <Form.Label>Title</Form.Label>
-                            <Form.Control type="text" placeholder="Enter title" required />
-                    </Form.Group>
-                </Col>
-            </Row>
-        </Stack>
-    </Form>
-  )
+    return (
+        <Form>
+            <Stack gap={4}>
+                <Row>
+                    <Col>
+                        <Form.Group controlId="title">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control type="text" required />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group controlId="title">
+                            <Form.Label>Tags</Form.Label>
+                            <CreatableReactSelect isMulti />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Form.Group controlId="markdown">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control required as="textarea" rows={15} />
+                </Form.Group>
+                <Stack direction='horizontal' gap={4} className='justify-content-end'>
+                    <Link to=" ..">
+                        <Button type='submit' variant='primary'>Save</Button>
+                        <Button type='button' variant='outline-secondary'>Cancel</Button>
+                    </Link>
+                </Stack>
+            </Stack>
+        </Form>
+    )
 }
 
 export default NoteForm
