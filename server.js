3a1d5e011ec813c526693f9df7428b22bc454e43
Domains18 <gibsonsgibson88@gmail.com>
2023-04-25 19:24:13 +0300

    application setup
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3500;
+const path = require('path');
 require('dotenv').config();
 
 
-
-
+//static files
+express.application.use('/', express.static(path.join(__dirname, 'public')));
 
 
 
