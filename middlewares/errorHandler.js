f59e240985ead59741ebc413f097836105c5e6e2
Domains18 <gibsonsgibson88@gmail.com>
2023-04-28 08:54:25 +0300

    error handler
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,10 @@
 const { logEvents } = require('./logger');
 
-const errorHandler = (err, req, res, next)=>{
+const errorHandler = (err, req, res, next) => {
     logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errorLogs.log');
     console.log(err.stack);
-    
+
+    const status = res.statusCode ? res.statusCode : 500;
+    res.status(status)
+    res.json({ message: err.message });
 }
